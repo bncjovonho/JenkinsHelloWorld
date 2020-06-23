0ed6efb9a053bdb6cb6f336810dd0b9afb448e1b
Loic Ho-Von <loic.ho-von@bnc.ca>
2020-06-22 22:21:50 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] var0) {
+        for(int var1 = 0; var1 < 1000; ++var1) {
+            System.out.println(System.currentTimeMillis());
+        }
+
+    }
+}
