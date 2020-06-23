1bf736891d46cfcc72949cedf9ea3c0afa6d228a
bncjovonho <67301269+bncjovonho@users.noreply.github.com>
2020-06-23 13:58:22 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] var0) {
-        for(int var1 = 0; var1 < 1000; ++var1) {
+        for(int var1 = 0; var1 < 1500; ++var1) {
             System.out.println(System.currentTimeMillis());
         }
 
